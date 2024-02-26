8dfb87dcc29c1256effed39735ebf1a30c23488a
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-26 10:07:11 +0530

    added accordian dummy data

---
+++ src/components/accordian/data.js
@@ -0,0 +1,30 @@
+const data = [
+    {
+      id: "1",
+      question: "What is React?",
+      answer: "React is a JavaScript library for building user interfaces."
+    },
+    {
+      id: "2",
+      question: "What is JSX?",
+      answer: "JSX is a syntax extension for JavaScript used with React to describe what the UI should look like."
+    },
+    {
+      id: "3",
+      question: "What is a component in React?",
+      answer: "A component is a reusable piece of code that can be composed together to build complex UIs."
+    },
+    {
+      id: "4",
+      question: "What is state in React?",
+      answer: "State is an object that holds data that influences the behavior of a component."
+    },
+    {
+      id: "5",
+      question: "What is a prop in React?",
+      answer: "Props (short for properties) are inputs to a component that it can use to render itself."
+    }
+  ];
+  
+  export default data;
+  
