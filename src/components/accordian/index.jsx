08f606c62068a9d513ab785e7ae05b7784c1a94f
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-28 12:17:24 +0530

    created a function for enabling mutliselection

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -4,13 +4,14 @@ import './styles.css';
 
 export default function Accordian() {
     const [selected, setSelected] = useState(null);
+    const [enableMultiSelection,setEnableMultiSelection] =useState(false);
     function handleSingleSelection(getCurrentId){
         setSelected(getCurrentId===selected ? null : getCurrentId);
     }
 
     return (
         <div className="wrapper">
-            <button></button>
+            <button>Enable Multi Selection</button>
             <div className="accordian">
                 {data && data.length > 0 ? 
                     data.map(dataItem => (
