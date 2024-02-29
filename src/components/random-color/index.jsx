2a8de82d73e2f9abb5c089e6a70e296e38c6fccc
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-29 20:56:23 +0530

    created a function to generate a random color as random color utility

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -3,9 +3,16 @@ import { useState } from "react";
 export default function RandomColor(){
     const[typeOfColor, setTypeOfColor]=useState('hex');
     const [color,setColor]=useState("#000000");
+    function randomColorUtility(length){
+        return Math.floor(Math.random()*length)
+    }
     function handleCreateRandomHexColor(){
         // #345678
-        const hex =[0,1,2,3,4,5,6,7,8,9,'A','B',"C","D","E","F"]
+        const hex =[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+        let hexColor="#";
+        for(let i=0;i<6;i++){
+            hexColor +=hex[randomColorUtility(hex.length)]
+        }
 
     }
     function handleCreateRandomRgbColor(){
