8f0f52ff7c6be14916f3e4a6c301d7273e851cd7
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-29 19:34:32 +0530

    created function for selecting the type :hex is set as default

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -1,4 +1,7 @@
+
+
 export default function RandomColor(){
+    const[typeOfColor, setTypeOfColor]=useSate('hex');
 return <div className="container">
     <button>Create HEX Color</button>
     <button>Create RGB Color</button>
