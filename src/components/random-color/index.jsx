e9365d4a20f3fa4c670f900d673166f76500e4b8
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-29 19:42:01 +0530

    styled the div

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -3,7 +3,11 @@
 export default function RandomColor(){
     const[typeOfColor, setTypeOfColor]=useSate('hex');
     const [color,setColor]=useState('#000000');
-return <div className="container">
+return <div style={{
+    width :'100vw',
+    height:'100vh',
+    background:'color',
+}}>
     <button>Create HEX Color</button>
     <button>Create RGB Color</button>
     <button>Generate Random Color</button>
