f2e4cce18ed3ffb6f006af8a2150f4bcf9ac86d3
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-29 19:55:23 +0530

    applied  onclick attribute as settypeofcolor

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -1,16 +1,22 @@
-
+import { useState } from "react";
 
 export default function RandomColor(){
-    const[typeOfColor, setTypeOfColor]=useSate('hex');
-    const [color,setColor]=useState('#000000');
+    const[typeOfColor, setTypeOfColor]=useState('hex');
+    const [color,setColor]=useState("#000000");
+    function handleCreateRandomHexColor(){
+
+    }
+    function handleCreateRandomRgbColor(){
+        
+    }
 return <div style={{
     width :'100vw',
     height:'100vh',
     background:'color',
 }}>
-    <button>Create HEX Color</button>
-    <button>Create RGB Color</button>
-    <button>Generate Random Color</button>
+    <button onClick={()=>setTypeOfColor('hex')}>Create HEX Color</button>
+    <button onClick={()=>setTypeOfColor('rgb')}>Create RGB Color</button>
+    <button onClick={typeOfColor==='hex' ? handleCreateRandomHexColor : handleCreateRandomRgbColor}>Generate Random Color</button>
 
 </div>
 }
