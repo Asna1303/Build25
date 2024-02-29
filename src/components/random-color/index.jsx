c2838bb5085eb827930f7bdf0d77e5b9ea9002ee
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-29 22:58:26 +0530

    added style to button

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -34,6 +34,18 @@ export default function RandomColor() {
             <button onClick={() => setTypeOfColor('hex')}>Create HEX Color</button>
             <button onClick={() => setTypeOfColor('rgb')}>Create RGB Color</button>
             <button onClick={typeOfColor === 'hex' ? handleCreateRandomHexColor : handleCreateRandomRgbColor}>Generate Random Color</button>
+            <div style={{
+                display: 'flex',
+                justifyContent:'center',
+                alignItems:'center',
+                color: 'white',
+                fontSize:'60px',
+                marginTop:'50px'
+
+            }}>
+                <h3>{typeOfColor}</h3>
+            <h1>{color}</h1>
+            </div>
         </div>
     );
 }
