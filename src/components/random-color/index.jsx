bc2e9d47c2428fdd4829af09dbf714e74948be2e
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-29 18:10:56 +0530

    created buttons for rgb color as well as hex color

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -1,5 +1,8 @@
 export default function RandomColor(){
 return <div className="container">
-<button>Generate Random Color</button>
+    <button>Create HEX Color</button>
+    <button>Create RGB Color</button>
+    <button>Generate Random Color</button>
+
 </div>
 }
