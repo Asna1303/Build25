247138ca1100dd409d15bca3bfa84ad74f246656
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-29 19:59:34 +0530

    CREATED HEX ARRAY

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -4,10 +4,12 @@ export default function RandomColor(){
     const[typeOfColor, setTypeOfColor]=useState('hex');
     const [color,setColor]=useState("#000000");
     function handleCreateRandomHexColor(){
+        // #345678
+        const hex =[0,1,2,3,4,5,6,7,8,9,'A','B',"C","D","E","F"]
 
     }
     function handleCreateRandomRgbColor(){
-        
+
     }
 return <div style={{
     width :'100vw',
