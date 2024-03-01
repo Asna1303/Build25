1888572f7c596a42bc57d4b0b083ddf8bd782fad
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-01 18:00:12 +0530

    created a function and passed getcurrentindex

--- src/components/star-rating/index.jsx
+++ src/components/star-rating/index.jsx
@@ -7,7 +7,9 @@ export default function starRating({noOfStars=5}){
 
     const [rating,setRating]=useState(0);
     const [hover,setHover]=useState(0);
-
+    function handleClick(getCurrentIndex){
+        
+    }
 return <div className="star-rating">{
     [...Array(noOfStars)].map((_,index)=>{
         return <FaStar
