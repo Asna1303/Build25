5b50351dcae7696a43372253234b592466607eca
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-01 18:22:27 +0530

    added classname to select either active or inactive

--- src/components/star-rating/index.jsx
+++ src/components/star-rating/index.jsx
@@ -1,34 +1,39 @@
+import { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import './styles.css';
 
-import { useState } from 'react';
-import { FaStar } from 'react-icons/fa';
+export default function StarRating({ noOfStars = 5 }) {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
 
+  function handleClick(getCurrentIndex) {
+    setRating(getCurrentIndex);
+  }
 
-export default function starRating({noOfStars=5}){
+  function handleMouseEnter(getCurrentIndex) {
+    setHover(getCurrentIndex);
+  }
 
-    const [rating,setRating]=useState(0);
-    const [hover,setHover]=useState(0);
-    function handleClick(getCurrentIndex){
-        setRating(getCurrentIndex);
-    }
-    function handleMouseEnter(getCurrentIndex){
-        setHover(getCurrentIndex);
-    }
-    function handleMouseLeave() {
-        setHover(rating);
-      }
-return <div className="star-rating">{
-    [...Array(noOfStars)].map((_,index)=>{
+  function handleMouseLeave() {
+    setHover(rating);
+  }
+
+  return (
+    <div className="star-rating">
+      {[...Array(noOfStars)].map((_, index) => {
         index += 1;
 
-        return <FaStar
-        key={index}
-        onClick={() => handleClick(index)}
-        onMouseMove={() => handleMouseEnter(index)}
-        onMouseLeave={() => handleMouseLeave()}
-        size={40}
-        />
-    })
-}
-
-</div>
+        return (
+          <FaStar
+            key={index}
+            className={index <= (hover || rating) ? "active" : "inactive"}
+            onClick={() => handleClick(index)}
+            onMouseMove={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave()}
+            size={40}
+          />
+        );
+      })}
+    </div>
+  );
 }
