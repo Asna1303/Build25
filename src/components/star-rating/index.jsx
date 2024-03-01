676b3cb1cedfaad5c82c62c8d7a2fba2c9d8f907
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-01 18:02:15 +0530

    created a function handlemouseEnter

--- src/components/star-rating/index.jsx
+++ src/components/star-rating/index.jsx
@@ -8,6 +8,9 @@ export default function starRating({noOfStars=5}){
     const [rating,setRating]=useState(0);
     const [hover,setHover]=useState(0);
     function handleClick(getCurrentIndex){
+
+    }
+    function handleMouseEnter(getCurrentIndex){
         
     }
 return <div className="star-rating">{
