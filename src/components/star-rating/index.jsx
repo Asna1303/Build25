0e331bdc11f2d6cdb244bd057836d1a0cbfc6d19
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-01 18:31:08 +0530

    star rating is ready

--- src/components/star-rating/index.jsx
+++ src/components/star-rating/index.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { FaStar } from "react-icons/fa";
 import './styles.css';
 
-export default function StarRating({ noOfStars = 5 }) {
+export default function StarRating({ noOfStars = 7 }) {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
