b9effabdf2b67ae60ae3dfef07e1000cbf32f40f
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-01 17:22:04 +0530

    imported fa star

--- src/components/star-rating/index.jsx
+++ src/components/star-rating/index.jsx
@@ -1,8 +1,14 @@
 
+import { FaStar } from 'react-icons/fa';
+
 
 export default function starRating({noOfStars=5}){
 return <div className="star-rating">{
-    [...Array(noOfStars)]
+    [...Array(noOfStars)].map((_,index)=>{
+        return <FaStar
+        
+        />
+    })
 }
 
 </div>
