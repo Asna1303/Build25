aef2df3bf92a9058d55423233b2120fd720dc7df
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-02 22:04:32 +0530

    created usestate for current slide and set to 0

--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 
 export default function ImageSlider({url,limit}){
     const[images,setImages]=useState([]);
+    const[currentSlide,setCurrentSlide]=useState(0);
     return <div className="container">
 
 </div>
