e3ec377e9faaf2d2b5c1b1484003c41d1411233b
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-03 13:38:10 +0530

    added anew state

--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -4,11 +4,12 @@ import { useState } from "react"
 export default function ImageSlider({url,limit}){
     const[images,setImages]=useState([]);
     const[currentSlide,setCurrentSlide]=useState(0);
+    const[errorMsg,setErrorMsg]=useState(null);
     async function fetchImages(getUrl){
         try{
 
         }catch(e){
-            
+
         }
 
     }
