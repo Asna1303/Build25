517f901ec23489ace622bc7a42e285d89301f131
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-09 16:10:07 +0530

    defined a usestate with products and setproducts with empty array

--- src/components/load-more-data/index.jsx
+++ src/components/load-more-data/index.jsx
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function LoadMoreData() {
     const [loading, setLoading] = useState(false);
+    const[products,setProducts]=useState([]);
 
     return <div className="container">
 
