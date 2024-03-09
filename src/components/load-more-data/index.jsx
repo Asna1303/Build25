e276fcc9fd8411a71e77a7bd87c7ce6e80c4662a
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-09 17:00:11 +0530

    defined button using usestate

--- src/components/load-more-data/index.jsx
+++ src/components/load-more-data/index.jsx
@@ -5,6 +5,7 @@ export default function LoadMoreData() {
     const [loading, setLoading] = useState(false);
     const[products,setProducts]=useState([]);
     const [count,setCount]=useState(0);
+    const [disableButton, setDisableButton] = useState(false);
 async function fetchProducts(){
 try{
     setLoading(true);
