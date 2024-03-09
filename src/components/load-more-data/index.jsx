db893b475c2cdd19e14315e0f1cb0669ed6607b6
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-09 16:11:39 +0530

    defined a usestate with count and setcount

--- src/components/load-more-data/index.jsx
+++ src/components/load-more-data/index.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 export default function LoadMoreData() {
     const [loading, setLoading] = useState(false);
     const[products,setProducts]=useState([]);
+    const [count,setCount]=useState(0);
 
     return <div className="container">
 
