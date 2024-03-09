9c50a74433714d7c307098a5f6e9244ea4b90962
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-09 16:57:55 +0530

    implemented a button

--- src/components/load-more-data/index.jsx
+++ src/components/load-more-data/index.jsx
@@ -45,5 +45,11 @@ fetchProducts()
     }
 
 </div>
+<div className="button-container">
+        <button disabled={disableButton} onClick={() => setCount(count + 1)}>
+          Load More Products
+        </button>
+        {disableButton ? <p>You have reached to 100 products</p> : null}
+      </div>
     </div>
 }
