75e44490c1da33fc7eb9fe89498c9d148a9db594
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-09 16:16:39 +0530

    called the fetchproducts and applied a try and catch

--- src/components/load-more-data/index.jsx
+++ src/components/load-more-data/index.jsx
@@ -4,7 +4,13 @@ export default function LoadMoreData() {
     const [loading, setLoading] = useState(false);
     const[products,setProducts]=useState([]);
     const [count,setCount]=useState(0);
+async function fetchProducts(){
+try{
 
+}catch(e){
+    
+}
+}
 
     useEffect(()=>{
 fetchProducts()
