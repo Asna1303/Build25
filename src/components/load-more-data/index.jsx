52445a0aac2b6e3a9da828661cf0b69b9f721a25
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-09 16:50:36 +0530

    set setloading as true in try

--- src/components/load-more-data/index.jsx
+++ src/components/load-more-data/index.jsx
@@ -7,6 +7,7 @@ export default function LoadMoreData() {
     const [count,setCount]=useState(0);
 async function fetchProducts(){
 try{
+    setLoading(true);
 const response = await fetch(`https://dummyjson.com/products?limit=20&skip=${
     count === 0 ? 0 : count * 20
   }`);
